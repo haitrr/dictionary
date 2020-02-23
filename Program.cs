827afa7398ba3a34144df37163538ae480ed8fb3
vroyibg <vroyibg@gmail.com>
2020-02-23 11:30:21 +0700

    add port config

--- Program.cs
+++ Program.cs
@@ -1,17 +1,28 @@
-﻿using Microsoft.AspNetCore;
+﻿using System.IO;
+using Microsoft.AspNetCore;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
 
 namespace Dictionary
 {
-    public class Program
+  public class Program
+  {
+    public static void Main(string[] args)
     {
-        public static void Main(string[] args)
-        {
-            CreateWebHostBuilder(args).Build().Run();
-        }
-
-        public static IWebHostBuilder CreateWebHostBuilder(string[] args) =>
-            WebHost.CreateDefaultBuilder(args)
-                .UseStartup<Startup>();
+      CreateWebHostBuilder(args).Build().Run();
     }
+
+    public static IWebHostBuilder CreateWebHostBuilder(string[] args)
+    {
+      var config = new ConfigurationBuilder()
+     .SetBasePath(Directory.GetCurrentDirectory())
+     .AddJsonFile("appsettings.json", optional: false)
+     .Build();
+
+      return WebHost.CreateDefaultBuilder(args)
+            .UseUrls($"http://localhost:{config.GetValue<int>("Host:Port")}")
+            .UseKestrel()
+            .UseStartup<Startup>();
+    }
+  }
 }
